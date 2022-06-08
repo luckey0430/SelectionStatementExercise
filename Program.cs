b0511524f604d3679014acadf7de56d1b6a15a78
EazyE <deron0117@gmail.com>
2022-06-08 11:29:00 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionStatementExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int favoriteNumber = 42;
+            Console.WriteLine("Guess the favorite number. Hint: my age");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favoriteNumber)
+            {
+                Console.WriteLine("Too LOW!!!");
+
+            }
+            else if (userGuess > favoriteNumber)
+            {
+                Console.WriteLine("Too HIGH!!!");
+
+            }
+            else if (userGuess == favoriteNumber)
+            {
+                Console.WriteLine("You were CORRECT!!!");
+
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+
+            }
+
+            string subject  = "c#";
+
+            Console.WriteLine("Choose a subject");
+            string userSubject = Console.ReadLine();
+
+            switch (userSubject)
+            {
+                case "c#":
+                    Console.WriteLine("You are in the right class!");
+                    break;
+                case "Java":
+                    Console.WriteLine("C# is better than Java");
+                    break;
+                    
+
+                default:
+                    Console.WriteLine("We teach coding here!");
+                    break;
+            }
+        }   
+       
+
+    }
+}
